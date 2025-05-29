5046dd58a855f5b20918cc79bf551e86f654902b
David Klassen <189554501+kladav@users.noreply.github.com>
2025-05-29 17:50:44 +0200

    Первая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const cardContainer = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+function addCard(card, deleteCard) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
 
-// @todo: Функция создания карточки
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardTitle.textContent = card.name;
 
-// @todo: Функция удаления карточки
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteCard(cardElement);
+  });
 
-// @todo: Вывести карточки на страницу
+  return cardElement;
+}
+
+function deleteCard(card) {
+  card.remove();
+}
+
+initialCards.forEach(function (card) {
+  cardContainer.append(addCard(card, deleteCard));
+});
